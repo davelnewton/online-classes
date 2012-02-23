8c0a162e92907400aa6c4626bfac7b740daa76f4
Dave Newton <davelnewton@gmail.com>
2012-02-23 12:23:51 -0500

    Created uniform probability function. Trying to remember Python :(

--- udacity/cs373/unit01/1.6-generalized-uniform-distribution.py
+++ udacity/cs373/unit01/1.6-generalized-uniform-distribution.py
@@ -1,4 +1,14 @@
-n=5
-p=[1.0/n]*n
+# Multiplying a single-element array creates 
+# a 1-d array containing the original element.
+
+# Initial code.
+n = 5
+p = [1.0 / n] * n
 
 print p
+
+# Returns a uniform probability array of size n.
+def uniform_probability(n):
+    return [1.0 / n] * n
+
+print uniform_probability(10)
